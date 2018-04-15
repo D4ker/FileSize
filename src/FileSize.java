f4d43ac75b940dd718fecfa9c5e42f1e9ff064de
D4ker <Alex-Daker@yandex.ru>
2018-04-16 00:16:06 +0300

    Added Main Class

--- src/FileSize.java
+++ src/FileSize.java
@@ -1,2 +1,32 @@
 public final class FileSize {
+
+    private final String[] filePath;
+    private final String[] size;
+
+    public FileSize(String line) {
+
+        // Получаем значения флагов и пути к файлам
+        final CommandParser treatedCommand = new CommandParser(line);
+
+        filePath = treatedCommand.getFilePath();
+        final boolean h = treatedCommand.getH();
+        final boolean c = treatedCommand.getC();
+        final boolean si = treatedCommand.getSi();
+
+        // Получаем размеры файлов в байтах
+        final SizeHandler receivedSize = new SizeHandler(c, filePath);
+
+        // Обрабатываем размеры файлов с учётом флагов
+        final FlagHandler treatedSize = new FlagHandler(h, si, receivedSize.getSize());
+
+        size = treatedSize.getSize();
+    }
+
+    public String[] getSize() {
+        return size;
+    }
+
+    public String[] getFilePath() {
+        return filePath;
+    }
 }
